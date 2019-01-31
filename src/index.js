5882e7e3aca8dc374ce544c7cc296db564f964ef
marisolcastrolopez <46985112+marisolcastrolopez@users.noreply.github.com>
2019-01-31 13:25:57 -0600

    Empecé mi fórmula para codificar con una sola letra usando ya el boilerplate original en mi nueva branch

---
+++ src/index.js
@@ -0,0 +1,10 @@
+let textToEncode = () => {
+	let cipherKey = 2;
+	let plainText= 'A';
+	let txtNum = plainText.charCodeAt();
+
+	let txtNumEncoded = (txtNum - 65 + cipherKey) % 26 + 65;
+	let encodedLetter = txtNumEncoded.fromCharCode();
+
+
+};
