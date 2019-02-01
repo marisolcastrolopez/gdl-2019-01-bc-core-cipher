d9cfeff6d1618fdfca6bd915af65f12c71a5e811
marisolcastrolopez <46985112+marisolcastrolopez@users.noreply.github.com>
2019-01-31 22:19:37 -0600

    Cipher funcionando para cifrar letras A-Z. Sólo cifra una letra. Cipher de test y control

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,42 @@
 window.cipher = {
-  
+  cipher.encode(cipherKey, plainText) {
+	
+  }
+  cipher.decode(offset, string){
+
+  }
 };
+/*Ciphering working with one letter*/
+var normalText = 'A';
+var cipherKey = '2'
+for (var i = 0; i < normalText.length; i++) {
+	var c = normalText.charCodeAt(i);
+	var result = []
+  if (65 <= c && c <=  90) {
+  result += String.fromCharCode((c - 65 + cipherKey) % 26 + 65);
+  }
+  else {
+  console.log('Hi');
+	};
+};
+
+/*JUST TESTS
+let text = 'ABCDEFG'
+
+let cipherKey = 2;
+var plainText= '';
+let plainTextSplitted = plainText.split('');
+let txtNum = plainTextSplitted.charCodeAt(plainText++);
+let txtNumEncoded = (txtNum - 65 + cipherKey) % 26 + 65;
+let encodedLetter = String.fromCharCode(txtNumEncoded);
+
+
+let plainText= 'ABCDEFG';
+let plainTextSplitted = plainText.split('');
+
+
+ let cipherKey = 2;
+		let plainText= 'ABC';
+		let txtNum = plainText.charCodeAt();
+		let txtNumEncoded = (txtNum - 65 + cipherKey) % 26 + 65;
+		let encodedLetter = String.fromCharCode(txtNumEncoded);*/
--- src/index.js
+++
@@ -1,9 +0,0 @@
-let textToEncode = () => {
-	let cipherKey = 2;
-	let plainText= 'A';
-	let txtNum = plainText.charCodeAt();
-	let txtNumEncoded = (txtNum - 65 + cipherKey) % 26 + 65;
-	let encodedLetter = String.fromCharCode(txtNumEncoded);
-
-
-};
